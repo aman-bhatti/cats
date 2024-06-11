2ec0ca9cdc8366cb9527eb2f128e8437f53b3d03
aman-bhatti <ai.bhatti13@gmail.com>
2024-06-10 23:52:11 -0400

    theo included

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let btn_generate = document.querySelector(".btn_generate");
+/*let btn_generate = document.querySelector(".btn_generate");
 
 btn_generate.addEventListener("click", getCats);
 
@@ -22,4 +22,60 @@ function getCats() {
         catsDiv.appendChild(catsEle)
     })
     .catch(err => console.log(err))
+}*/
+
+function getLocalCats() {
+  // Array of local image paths
+  let localCats = [
+    "pictures/theo1.jpg",
+    "pictures/theo2.jpg",
+    "pictures/theo3.jpg",
+    "pictures/theo4.jpg",
+    "pictures/theo5.jpg",
+    "pictures/theo6.jpg",
+  ];
+
+  let catsDiv = document.querySelector(".catPics");
+
+  // Pick a random local image
+  let randomIndex = Math.floor(Math.random() * localCats.length);
+  let src = localCats[randomIndex];
+
+  let img = document.createElement("img");
+  img.setAttribute("src", src);
+  img.classList.add("pictures");
+  catsDiv.appendChild(img);
 }
+
+function getApiCat() {
+  fetch(
+    "https://api.thecatapi.com/v1/images/search?api_key=live_Y8AIqRsILMaCpE1pCO6seI7drCSNOLOz3AB2TpALsyCbRWIZmGtvfhgHvgLzpBWr",
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let catsImg = data[0].url;
+
+      let catsEle = document.createElement("img");
+      catsEle.setAttribute("src", `${catsImg}`);
+      catsEle.classList.add("pictures");
+
+      let catsDiv = document.querySelector(".catPics");
+      catsDiv.appendChild(catsEle);
+    })
+    .catch((err) => console.log(err));
+}
+
+function getCats() {
+  let catsDiv = document.querySelector(".catPics");
+  catsDiv.innerHTML = "";
+
+  // Randomly decide whether to show a local cat image or an API cat image
+  if (Math.random() < 0.5) {
+    getLocalCats();
+  } else {
+    getApiCat();
+  }
+}
+
+let btn_generate = document.querySelector(".btn_generate");
+btn_generate.addEventListener("click", getCats);
